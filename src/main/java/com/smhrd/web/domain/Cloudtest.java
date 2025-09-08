5efda9da26c3bacd8bb2e45b4831e2268525544f
yeonii <syh040405@gmail.com>
2025-09-08 14:20:17 +0900

    commit message

--- src/main/java/com/smhrd/web/domain/Cloudtest.java
+++ src/main/java/com/smhrd/web/domain/Cloudtest.java
@@ -6,13 +6,13 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-@Entity
+@Entity //Entity 무조건 있어야 함
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class Cloudtest {
 	
-	@Id
+	@Id // 절대 빠지면 안됨 (primary key)
 	private Long id;
 	private String name;
 	private String gender;
